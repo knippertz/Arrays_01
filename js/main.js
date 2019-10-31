b335b502b40ef86f9d5412a019bcc8fd11b4cc4d
knippertz <klausknippertz@gmail.com>
2019-10-31 13:32:46 +0100

    Arrays_01

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+var heroUndEnemy = [["Batman", "The Joker"],["Professor X", "Magneto"],["Thor", "Loki"]]
+
+heroUndEnemy.push(["Superman", "Lex"]);
+heroUndEnemy.push(["Wolverine", "Sabretooth"]);
+heroUndEnemy.push(["Ghost", "mMphisto"]);
+
+console.log(heroUndEnemy);
+
